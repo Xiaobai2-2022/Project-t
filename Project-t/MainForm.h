b08cf5ca8d9fb1d4560ea6ac436d38977861b5a1
z2367li <z2367li@uwaterloo.ca>
2022-10-13 02:04:19 -0400

    Colors! Added method to display a line (L-block)

--- Project-t/MainForm.h
+++ Project-t/MainForm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <random>
 
 namespace Projectt {
 
@@ -43,7 +44,9 @@ namespace Projectt {
 
 		static const int size_form_x = left_offset + size_block * num_col + right_offset;			// main form's x size
 		static const int size_form_y = size_border * 2 + size_block * num_row;						// main form's Y size
-		
+
+		static const int num_type = 7;
+		static const int num_next = 5;
 
 	/* private helper methods */
 	private:
@@ -54,7 +57,7 @@ namespace Projectt {
 		 * @returns: (int) the one dimensional code
 		 */
 		static int to_row_major (int x, int y, int row_size) {
-			return row_size * x + y;
+			return row_size * y + x;
 		}
 
 	public:
@@ -64,6 +67,8 @@ namespace Projectt {
 			//
 			//TODO: Add the constructor code here
 			//
+			this->next_block[0] = 0;
+			this->update_right_main();
 		}
 
 	protected:
@@ -77,8 +82,8 @@ namespace Projectt {
 				delete components;
 			}
 		} 
+	// All labels
 	private: 
-
 		array<System::Windows::Forms::Label^>^ labels = gcnew array<System::Windows::Forms::Label^>(num_lbl);
 		array<System::Windows::Forms::Label^>^ labels_left = gcnew array<System::Windows::Forms::Label^>(num_lbl_s);
 		array<System::Windows::Forms::Label^>^ labels_right_main = gcnew array<System::Windows::Forms::Label^>(num_lbl_s);
@@ -94,6 +99,10 @@ namespace Projectt {
 		System::Windows::Forms::Label^ label_right_next_3rd;
 		System::Windows::Forms::Label^ label_right_next_4th;
 		System::Windows::Forms::Label^ label_right_next_5th;
+
+	// All logic fields
+	private: 
+		array<int>^ next_block = gcnew array<int>(num_next);
 
 	private:
 		/// <summary>
@@ -142,11 +151,10 @@ namespace Projectt {
 			this->SuspendLayout();
 
 			/* Set up the labels main */
-			for (int i = 0; i < num_col; i++) {
-				for (int j = 0; j < num_row; j++) {
+			for (int i = 0; i < num_row; i++) {
+				for (int j = 0; j < num_col; j++) {
 					this->labels[to_row_major(i, j, num_row)]->AutoSize = false;
-					this->labels[to_row_major(i, j, num_row)]->Location = System::Drawing::Point(left_offset + i * size_block, size_border + j * size_block);						// calculate the location for the block
-					this->labels[to_row_major(i, j, num_row)]->Name = L"label" + i;
+					this->labels[to_row_major(i, j, num_row)]->Location = System::Drawing::Point(left_offset + j * size_block, size_border + i * size_block);						// calculate the location for the block
 					this->labels[to_row_major(i, j, num_row)]->Size = System::Drawing::Size(size_block, size_block);																// set each block's size
 					this->labels[to_row_major(i, j, num_row)]->TabIndex = 0;
 					this->labels[to_row_major(i, j, num_row)]->Text = L"";
@@ -156,10 +164,9 @@ namespace Projectt {
 			}
 
 			/* Set up the labels left */
-			for (int i = 0; i < num_col_s; i++) {
-				for (int j = 0; j < num_row_s; j++) {
-					this->labels_left[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_border + i * size_disp_block, size_border + j * size_disp_block);	// calculate the location for the block
-					this->labels_left[to_row_major(i, j, num_row_s)]->Name = L"label" + i;
+			for (int i = 0; i < num_row_s; i++) {
+				for (int j = 0; j < num_col_s; j++) {
+					this->labels_left[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_border + j * size_disp_block, size_border + i * size_disp_block);	// calculate the location for the block
 					this->labels_left[to_row_major(i, j, num_row_s)]->Size = System::Drawing::Size(size_disp_block, size_disp_block);												// set each block's size
 					this->labels_left[to_row_major(i, j, num_row_s)]->TabIndex = 0;
 					this->labels_left[to_row_major(i, j, num_row_s)]->Text = L"";
@@ -169,10 +176,9 @@ namespace Projectt {
 			}
 
 			/* Set up the labels right main */
-			for (int i = 0; i < num_col_s; i++) {
-				for (int j = 0; j < num_row_s; j++) {
-					this->labels_right_main[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset + size_border * 3 + i * size_disp_block, size_border + j * size_disp_block);	// calculate the location for the block
-					this->labels_right_main[to_row_major(i, j, num_row_s)]->Name = L"label" + i;
+			for (int i = 0; i < num_row_s; i++) {
+				for (int j = 0; j < num_col_s; j++) {
+					this->labels_right_main[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset + size_border * 3 + j * size_disp_block, size_border + i * size_disp_block);	// calculate the location for the block
 					this->labels_right_main[to_row_major(i, j, num_row_s)]->Size = System::Drawing::Size(size_disp_block, size_disp_block);																				// set each block's size
 					this->labels_right_main[to_row_major(i, j, num_row_s)]->TabIndex = 0;
 					this->labels_right_main[to_row_major(i, j, num_row_s)]->Text = L"";
@@ -182,10 +188,9 @@ namespace Projectt {
 			}
 
 			/* Set up the labels right 2nd */
-			for (int i = 0; i < num_col_s; i++) {
-				for (int j = 0; j < num_row_s; j++) {
-					this->labels_right_2nd[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset_s + size_border * 3 + i * size_small_block, size_border + j * size_small_block + 4 * size_disp_block + size_border);	// calculate the location for the block
-					this->labels_right_2nd[to_row_major(i, j, num_row_s)]->Name = L"label" + i;
+			for (int i = 0; i < num_row_s; i++) {
+				for (int j = 0; j < num_col_s; j++) {
+					this->labels_right_2nd[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset_s + size_border * 3 + j * size_small_block, size_border + i * size_small_block + 4 * size_disp_block + size_border);	// calculate the location for the block
 					this->labels_right_2nd[to_row_major(i, j, num_row_s)]->Size = System::Drawing::Size(size_small_block, size_small_block);																													// set each block's size
 					this->labels_right_2nd[to_row_major(i, j, num_row_s)]->TabIndex = 0;
 					this->labels_right_2nd[to_row_major(i, j, num_row_s)]->Text = L"";
@@ -195,10 +200,9 @@ namespace Projectt {
 			}
 
 			/* Set up the labels right 3rd */
-			for (int i = 0; i < num_col_s; i++) {
-				for (int j = 0; j < num_row_s; j++) {
-					this->labels_right_3rd[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset_s + size_border * 3 + i * size_small_block, size_border + j * size_small_block + 4 * size_disp_block + 3 * size_small_block + 2 * size_border);	// calculate the location for the block
-					this->labels_right_3rd[to_row_major(i, j, num_row_s)]->Name = L"label" + i;
+			for (int i = 0; i < num_row_s; i++) {
+				for (int j = 0; j < num_col_s; j++) {
+					this->labels_right_3rd[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset_s + size_border * 3 + j * size_small_block, size_border + i * size_small_block + 4 * size_disp_block + 3 * size_small_block + 2 * size_border);	// calculate the location for the block
 					this->labels_right_3rd[to_row_major(i, j, num_row_s)]->Size = System::Drawing::Size(size_small_block, size_small_block);																																			// set each block's size
 					this->labels_right_3rd[to_row_major(i, j, num_row_s)]->TabIndex = 0;
 					this->labels_right_3rd[to_row_major(i, j, num_row_s)]->Text = L"";
@@ -208,10 +212,9 @@ namespace Projectt {
 			}
 
 			/* Set up the labels right 4th */
-			for (int i = 0; i < num_col_s; i++) {
-				for (int j = 0; j < num_row_s; j++) {
-					this->labels_right_4th[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset_s + size_border * 3 + i * size_small_block, size_border + j * size_small_block + 4 * size_disp_block + 6 * size_small_block + 3 * size_border);	// calculate the location for the block
-					this->labels_right_4th[to_row_major(i, j, num_row_s)]->Name = L"label" + i;
+			for (int i = 0; i < num_row_s; i++) {
+				for (int j = 0; j < num_col_s; j++) {
+					this->labels_right_4th[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset_s + size_border * 3 + j * size_small_block, size_border + i * size_small_block + 4 * size_disp_block + 6 * size_small_block + 3 * size_border);	// calculate the location for the block
 					this->labels_right_4th[to_row_major(i, j, num_row_s)]->Size = System::Drawing::Size(size_small_block, size_small_block);																																			// set each block's size
 					this->labels_right_4th[to_row_major(i, j, num_row_s)]->TabIndex = 0;
 					this->labels_right_4th[to_row_major(i, j, num_row_s)]->Text = L"";
@@ -221,10 +224,9 @@ namespace Projectt {
 			}
 
 			/* Set up the labels right 5th */
-			for (int i = 0; i < num_col_s; i++) {
-				for (int j = 0; j < num_row_s; j++) {
-					this->labels_right_5th[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset_s + size_border * 3 + i * size_small_block, size_border + j * size_small_block + 4 * size_disp_block + 9 * size_small_block + 4 * size_border);	// calculate the location for the block
-					this->labels_right_5th[to_row_major(i, j, num_row_s)]->Name = L"label" + i;
+			for (int i = 0; i < num_row_s; i++) {
+				for (int j = 0; j < num_col_s; j++) {
+					this->labels_right_5th[to_row_major(i, j, num_row_s)]->Location = System::Drawing::Point(size_form_x - right_offset_s + size_border * 3 + j * size_small_block, size_border + i * size_small_block + 4 * size_disp_block + 9 * size_small_block + 4 * size_border);	// calculate the location for the block
 					this->labels_right_5th[to_row_major(i, j, num_row_s)]->Size = System::Drawing::Size(size_small_block, size_small_block);																																			// set each block's size
 					this->labels_right_5th[to_row_major(i, j, num_row_s)]->TabIndex = 0;
 					this->labels_right_5th[to_row_major(i, j, num_row_s)]->Text = L"";
@@ -236,7 +238,6 @@ namespace Projectt {
 			/* HOLD label */
 			this->label_left_hold->AutoSize = false;
 			this->label_left_hold->Location = System::Drawing::Point(size_border, size_border + 3 * size_disp_block);																// set the location for the block
-			this->label_left_hold->Name = L"label_hold";
 			this->label_left_hold->Size = System::Drawing::Size(size_disp_block * 4, size_disp_block);																				// set the block's size
 			this->label_left_hold->TabIndex = 0;
 			this->label_left_hold->Text = L"HOLD";
@@ -246,7 +247,6 @@ namespace Projectt {
 			/* SCORE label */
 			this->label_left_score->AutoSize = false;
 			this->label_left_score->Location = System::Drawing::Point(size_border, up_offset - size_block);																			// set the location for the block
-			this->label_left_score->Name = L"label_score";
 			this->label_left_score->Size = System::Drawing::Size(size_disp_block * 4, size_disp_block);																				// set the block's size
 			this->label_left_score->TabIndex = 0;
 			this->label_left_score->Text = L"SCORE";
@@ -256,7 +256,6 @@ namespace Projectt {
 			/* SCORE Display label */
 			this->label_left_score_disp->AutoSize = false;
 			this->label_left_score_disp->Location = System::Drawing::Point(size_border, up_offset);																					// set the location for the block
-			this->label_left_score_disp->Name = L"label_score_disp";
 			this->label_left_score_disp->Size = System::Drawing::Size(size_disp_block * 4, size_disp_block);																		// set the block's size
 			this->label_left_score_disp->TabIndex = 0;
 			this->label_left_score_disp->Text = L"" + 0;
@@ -267,7 +266,6 @@ namespace Projectt {
 			/* NEXT label */
 			this->label_right_next->AutoSize = false;
 			this->label_right_next->Location = System::Drawing::Point(size_form_x - right_offset + 3 * size_border, size_border + 3 * size_disp_block);								// set the location for the block
-			this->label_right_next->Name = L"label_next";
 			this->label_right_next->Size = System::Drawing::Size(size_disp_block * 4, size_disp_block);																				// set the block's size
 			this->label_right_next->TabIndex = 0;
 			this->label_right_next->Text = L"NEXT";
@@ -277,7 +275,6 @@ namespace Projectt {
 			/* 2nd label */
 			this->label_right_next_2nd->AutoSize = false;
 			this->label_right_next_2nd->Location = System::Drawing::Point(size_form_x - right_offset + 3 * size_border, 4 * size_disp_block + 2 * size_border);							// set the location for the block
-			this->label_right_next_2nd->Name = L"label_2nd";
 			this->label_right_next_2nd->Size = System::Drawing::Size(size_leftover - 2 * size_border, 3 * size_small_block);															// set the block's size
 			this->label_right_next_2nd->TabIndex = 0;
 			this->label_right_next_2nd->Text = L"2nd";
@@ -287,7 +284,6 @@ namespace Projectt {
 			/* 3rd label */
 			this->label_right_next_3rd->AutoSize = false;
 			this->label_right_next_3rd->Location = System::Drawing::Point(size_form_x - right_offset + 3 * size_border, 4 * size_disp_block + 3 * size_small_block + 3 * size_border);	// set the location for the block
-			this->label_right_next_3rd->Name = L"label_3rd";
 			this->label_right_next_3rd->Size = System::Drawing::Size(size_leftover - 2 * size_border, 3 * size_small_block);															// set the block's size
 			this->label_right_next_3rd->TabIndex = 0;
 			this->label_right_next_3rd->Text = L"3rd";
@@ -297,7 +293,6 @@ namespace Projectt {
 			/* 4th label */
 			this->label_right_next_4th->AutoSize = false;
 			this->label_right_next_4th->Location = System::Drawing::Point(size_form_x - right_offset + 3 * size_border, 4 * size_disp_block + 6 * size_small_block + 4 * size_border);	// set the location for the block
-			this->label_right_next_4th->Name = L"label_4th";
 			this->label_right_next_4th->Size = System::Drawing::Size(size_leftover - 2 * size_border, 3 * size_small_block);															// set the block's size
 			this->label_right_next_4th->TabIndex = 0;
 			this->label_right_next_4th->Text = L"4th";
@@ -307,7 +302,6 @@ namespace Projectt {
 			/* 5th label */
 			this->label_right_next_5th->AutoSize = false;
 			this->label_right_next_5th->Location = System::Drawing::Point(size_form_x - right_offset + 3 * size_border, 4 * size_disp_block + 9 * size_small_block + 5 * size_border);	// set the location for the block
-			this->label_right_next_5th->Name = L"label_5th";
 			this->label_right_next_5th->Size = System::Drawing::Size(size_leftover - 2 * size_border, 3 * size_small_block);															// set the block's size
 			this->label_right_next_5th->TabIndex = 0;
 			this->label_right_next_5th->Text = L"5th";
@@ -358,6 +352,38 @@ namespace Projectt {
 			this->PerformLayout();
 
 		}
+
+	// undate UI
+	private:
+		void update_right_main() {
+			for (int i = 0; i < this->labels_right_main->Length; i++) {
+				this->labels_right_main[i]->BackColor = Color::White;
+			}
+			switch(this->next_block[0]){
+			case 0:
+				this->labels_right_main[to_row_major(1, 0, num_row_s)]->BackColor = Color::LightSkyBlue;
+				this->labels_right_main[to_row_major(1, 1, num_row_s)]->BackColor = Color::LightSkyBlue;
+				this->labels_right_main[to_row_major(1, 2, num_row_s)]->BackColor = Color::LightSkyBlue;
+				this->labels_right_main[to_row_major(1, 3, num_row_s)]->BackColor = Color::LightSkyBlue;
+
+			}
+		}
+
+	// logic
+	//private:
+	//	// update the UI for the left
+	//	void update_UI() {
+	//	}
+	//	int init_block() {
+	//		for (int i = 0; i < num_next; i++) {
+	//			this->next_block[i] = std::rand() % num_type;
+	//		}
+	//	}
+	//	int load_block() {
+
+	//	}
+
+
 #pragma endregion
 	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
